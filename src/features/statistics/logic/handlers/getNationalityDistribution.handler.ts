60dcd003677424df7086e9e91a0e50898e95d7d4
Amr Khaled <lewolfje@gmail.com>
2024-06-15 17:16:44 +0300

    query for nationality distibution

---
+++ src/features/statistics/logic/handlers/getNationalityDistribution.handler.ts
@@ -0,0 +1,40 @@
+import { StudentModel } from "@fcai-sis/shared-models";
+import { Request, Response } from "express";
+
+/*
+ * Get an average distribution of students' nationalities (egyptian, foreign)
+ * */
+const handler = async (req: Request, res: Response) => {
+  const students = await StudentModel.find();
+  // filter egyptian students and foreign students
+  const egyptianStudents = students.filter(
+    (student) => student.nationality === "egyptian"
+  );
+  const foreignStudents = students.filter(
+    (student) => student.nationality === "foreigner"
+  );
+
+  // get the number of egyptian students and foreign students
+  const numberOfEgyptianStudents = egyptianStudents.length;
+  const numberOfForeignStudents = foreignStudents.length;
+
+  // get the percentage of egyptian students and foreign students
+  const totalNumberOfStudents = students.length;
+  const percentageOfEgyptianStudents =
+    (numberOfEgyptianStudents / totalNumberOfStudents) * 100;
+  const percentageOfForeignStudents =
+    (numberOfForeignStudents / totalNumberOfStudents) * 100;
+
+  const response = {
+    numberOfEgyptianStudents,
+    numberOfForeignStudents,
+    percentageOfEgyptianStudents,
+    percentageOfForeignStudents,
+  };
+
+  return res.status(200).json(response);
+};
+
+const getNationalityDistHandler = handler;
+
+export default getNationalityDistHandler;
