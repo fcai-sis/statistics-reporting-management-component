805eba498db9b6443143364227757b488ec06ef2
Amr Khaled <lewolfje@gmail.com>
2024-06-16 12:18:57 +0300

    more rawtes

--- src/features/statistics/statistics.routes.ts
+++ src/features/statistics/statistics.routes.ts
@@ -4,6 +4,8 @@ import { asyncHandler } from "@fcai-sis/shared-utilities";
 import getAllStudentsHandler from "./logic/handlers/getAllStudents.handler";
 import { paginationQueryParamsMiddleware } from "@fcai-sis/shared-middlewares";
 import getNationalityDistHandler from "./logic/handlers/getNationalityDistribution.handler";
+import getSuccessRateOfCourseHandler from "./logic/handlers/getSuccessRateOfCourse.handler";
+import getTotalHoursOfStudentHandler from "./logic/handlers/getTotalHoursOfStudent.handler";
 
 const statisticsRoutes = (router: Router) => {
   router.get(
@@ -12,10 +14,17 @@ const statisticsRoutes = (router: Router) => {
     asyncHandler(getAllStudentsHandler)
   );
 
+  router.get("/get-nationality-dist", asyncHandler(getNationalityDistHandler));
+
   router.get(
-    "/get-nationality-dist",
-    asyncHandler(getNationalityDistHandler)
-  )
+    "/get-success-rate-of-course/:courseId",
+    asyncHandler(getSuccessRateOfCourseHandler)
+  );
+
+  router.get(
+    "/get-total-hours-of-student/:studentId",
+    asyncHandler(getTotalHoursOfStudentHandler)
+  );
 };
 
 export default statisticsRoutes;
